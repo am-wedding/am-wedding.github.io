7f1fdc8ed5b336070e22c1461722f436bc9739db
asushakov <asushakov@pnblab.com>
2023-06-15 15:27:32 +0700

    site1

--- template/js/script.js
+++ template/js/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-  console.log('loaded!');
-  AOS.init();
-
+AOS.init({
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
 });
